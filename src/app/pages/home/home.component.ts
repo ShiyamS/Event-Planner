a78b059cb7ee729b12ea2738d45d6fbd4b7ddc1c
Shiyam <shiyams315@gmail.com>
2023-11-21 23:16:37 +0530

    Event Planner - Login and SignUp Modal

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, Renderer2 } from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  isLoginView: boolean = true;
+  @ViewChild('modal', { static: true }) myElement!: ElementRef;
+
+  constructor(private renderer: Renderer2, private el: ElementRef) { }
+
+  openLogin() {
+    const modal = this.myElement.nativeElement;
+    console.log(modal)
+    if (modal != null) {
+      this.renderer.setStyle(modal, 'display', 'block')
+    }
+  }
+
+  closeModal() {
+    const modal = this.myElement.nativeElement;
+    console.log(modal)
+    if (modal != null) {
+      this.renderer.setStyle(modal, 'display', 'none')
+    }
+  }
 
 }
